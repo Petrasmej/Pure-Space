963bde8b6d8171a354ae377dae7da1c39e7b5d39
Petra Smejkalova <smejkalova.petra@gmail.com>
2025-02-12 17:14:45 +0100

    player

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,22 +1,32 @@
 import { useRef } from 'react';
+import PropTypes from 'prop-types'; // Import prop-types
 
 const AudioPlayer = ({ src, title }) => {
   const audioRef = useRef(null);
 
   const playAudio = () => {
     if (audioRef.current) {
-      audioRef.current.play();
+      audioRef.current
+        .play()
+        .catch((error) => console.error('Chyba v přehrávání:', error));
     }
   };
 
   return (
     <div className="audio-container">
+      <h3>{title}</h3>
       <button className="button" onClick={playAudio}>
         ▶ Přehrát Joga Nidra
       </button>
-      <audio ref={audioRef} src={src} preload="auto" />
+      <audio ref={audioRef} src={src} preload="auto" controls />
     </div>
   );
+};
+
+// Přidání propTypes validace
+AudioPlayer.propTypes = {
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string,
 };
 
 export default AudioPlayer;
